68288282fda2ad06851bec837bdea8ad549827f1
Kieran Coughlan <KieranCoughlan@gmail.com>
2018-09-29 13:50:19 +0100

    Week02

---
+++ Template/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(400, 400);
+    background("LightSkyBlue");
+
+}
+
+function draw() {
+}
---
+++ Week02/script.js
@@ -0,0 +1,18 @@
+let a = 4;
+let b = 5;
+let c = Divide(a, b);
+
+if (c < 1){
+  MyFunction("c is less than one")
+}
+else {
+  MyFunction("c is greater than one")
+}
+
+function MyFunction(what){
+    console.log(what);
+}
+
+function Divide(n, m){
+    return n / m;
+}
