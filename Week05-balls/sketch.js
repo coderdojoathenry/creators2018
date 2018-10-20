011c13238d4fb379865b34a36d0e40ee8c8a7347
Mark Davis <mark_davis2@hpe.com>
2018-10-20 13:36:14 +0100

    Animated ball patterns

---
+++ Week05-balls/sketch.js
@@ -0,0 +1,40 @@
+let ballX;
+let ballY;
+let ballSize;
+let ballXSpeed=5;
+let ballYSpeed=1;
+
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+    ballX = width/2;
+    ballY = height/2;
+    ballSize=100;
+    background("LightSkyBlue");
+}
+
+function draw() {
+    // move the ball
+    ballX = ballX + ballXSpeed;
+    ballY = ballY + ballYSpeed;
+
+    // if the ball is near edge, bounce
+    if ((ballX+(ballSize/2) > width) || (ballX-(ballSize/2)<0)) {
+        ballXSpeed = ballXSpeed*-1;
+    }
+    if ((ballY+(ballSize/2) > height) || (ballY-(ballSize/2)<0)) {
+        ballYSpeed = ballYSpeed*-1;
+    }    
+
+    // Draw first ball
+    fill(mouseX, mouseY, 100, 100);
+    ellipse(ballX, ballY, ballSize, ballSize);
+  
+}
+
+function mousePressed(){
+    console.log("You clicked me!");
+    ballXSpeed = random(-5,5);
+    ballYSpeed = random(-5,5);
+    ballX = mouseX;
+    ballY = mouseY;
+}
