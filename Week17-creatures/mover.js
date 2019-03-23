f7d039358f528c39fdd37f4bc946569c3b353200
Mark Davis <mark_davis2@hpe.com>
2019-03-23 14:04:03 +0000

    Creature Art

---
+++ Week17-creatures/mover.js
@@ -0,0 +1,77 @@
+class Mover{
+    constructor(size, position){
+        this.size=size;
+        this.mass=this.size;
+        this.position=position;
+        this.home = position.copy();
+        this.acceleration=createVector(0,0);
+        this.velocity=createVector(0,0);
+        this.isAfraid = false;
+        this.maxSpeed = 5;
+    }
+
+    move(){
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0);
+    }
+
+    applyBehaviours(){
+        this.flee();
+        this.goHome();
+    }
+
+    goHome(){
+        // i want to go home
+        let desired = p5.Vector.sub(this.home, this.position);
+        desired = desired.limit(this.maxSpeed);
+        let steering = p5.Vector.sub(desired, this.velocity);
+        this.applyForce(steering);
+    }
+
+    flee(){
+        // i am scared of the mouse
+        let mousePosition = createVector(mouseX, mouseY);
+        let desired = p5.Vector.sub(this.position, mousePosition);
+        let dist = desired.mag();
+        if(dist < 100){
+            desired = desired.limit(this.maxSpeed);
+            let steering = p5.Vector.sub(desired, this.velocity);
+            this.applyForce(steering.mult(10));
+            this.isAfraid=true;
+        } else {
+            this.isAfraid = false;
+        }
+    }
+
+    show(){
+        if(this.isAfraid){
+            fill(255, 0, 0, 50);            
+        } else {
+            fill(0, 255, 0, 50);
+        }
+
+        ellipse(this.position.x, this.position.y, 
+            this.size, this.size);
+    }
+
+    applyForce(force){
+        let accelToAdd=force.copy().div(this.mass);
+        this.acceleration.add(accelToAdd);
+    }
+
+    checkEdges(){
+        if(this.position.x>width){
+            this.position.x=width;
+        }
+        if(this.position.x<0){
+            this.position.x=0;
+        }        
+        if(this.position.y<0){
+            this.position.y=0;
+        }                
+        if(this.position.y>height){
+            this.position.y=height;
+        }                        
+    }
+}
---
+++ Week17-creatures/sketch.js
@@ -0,0 +1,31 @@
+let movers=[];
+let font;
+let points = [];
+
+function preload(){
+    font = loadFont("fonts/AvenirNextLTW01-Medium.ttf");
+}
+
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+    textFont(font);
+    points = font.textToPoints("Creators", 100, height/2, 300);
+    
+    for(let i=0; i< points.length; i+=2){
+        let startPosition=createVector(points[i].x, points[i].y);
+        movers.push(new Mover(20, startPosition));
+    }
+}
+
+function draw() {
+    background("LightSkyBlue");  
+    fill(255);
+    ellipse(mouseX, mouseY, 50, 50);  
+
+    for(let i=0; i< movers.length; i++){   
+        movers[i].applyBehaviours(); 
+        movers[i].move();
+        movers[i].checkEdges();
+        movers[i].show();    
+    }    
+}
