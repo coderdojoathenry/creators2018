b24663ecf8a88a07e064a2b64ba7890538b5802b
Mark Davis <mark_davis2@hpe.com>
2019-04-06 15:47:47 +0100

    Added Drum Town

--- Week19-drumtown/sketch.js
+++ Week19-drumtown/sketch.js
@@ -1,22 +1,76 @@
 let car;
 let windows = [];
+let NUM_FLOORS = 7;
+let NUM_COLUMNS = 8;
+let sounds = [];
+
+function preload() {
+    for (let i = 0; i < NUM_FLOORS; i++) {
+        sounds.push(loadSound("sounds/" + i + ".wav"));
+    }
+}
 
 function setup() {
     createCanvas(800, 800);
     car = new Car();
 
-    windows.push(new Window(300, 400));
+    // Create a grid of windows
+    let columnSpace = width / NUM_COLUMNS;
+    for (let i = 0; i < NUM_COLUMNS; i++) {
+        for (let j = 0; j < NUM_FLOORS; j++) {
+            windows.push(new Window(columnSpace / 2 + columnSpace * i, 80 + j * 80, sounds[j]));
+        }
+    }
+
 }
 
 function draw() {
-    background("LightSkyBlue");    
+    background("LightSkyBlue");
+
+    // draw building, road
+    push();
+    fill(50)
+    rect(15, 20, width - 30, height - 100);
+    fill(100)
+    rect(0, height - 100, width, 150);
+    pop();
 
     // draw car
     car.move();
     car.show();
 
-    // draw window
-    for(let i=0; i< windows.length; i++){
+    // draw windows
+    for (let i = 0; i < windows.length; i++) {
+        if (windows[i].isActive && car.x > windows[i].x) {
+            windows[i].playSound();
+        }
+        if (car.x == 0) {
+            windows[i].hasPlayed = false;
+        }
         windows[i].show();
     }
+    showFadingMessage();
+}
+
+function mouseClicked() {
+    for (let i = 0; i < windows.length; i++) {
+        if (windows[i].touchingMouse()) {
+            windows[i].isActive = !windows[i].isActive;
+        }
+    }
+}
+
+function showFadingMessage(){
+    // show message that fades
+    push();
+    if(frameCount<200){
+        fill(255, 255, 0, 255-frameCount*1.25);
+        textSize(150);
+        textAlign(CENTER);
+        text("Drumtown", width/2, height/2);
+        textSize(50);
+        text("Click on windows to play", width/2, height/2 + 100);
+        
+    }
+    pop();
 }
--- Week19-drumtown/window.js
+++ Week19-drumtown/window.js
@@ -1,14 +1,45 @@
 class Window {
-    constructor (x, y){
+    constructor(x, y, sound) {
         this.x = x;
         this.y = y;
+        this.sound = sound;
+        this.isActive = false;
+        this.hasPlayed = false;
+        this.strokeWeight=3;
     }
 
-    show(){
+    touchingMouse() {
+        if (mouseX < this.x - 25
+            || mouseX > this.x + 25) {
+            return (false);
+        }
+        if (mouseY < this.y - 25
+            || mouseY > this.y + 25) {
+            return (false);
+        }
+        return (true);
+    }
+
+    playSound() {
+        if (this.hasPlayed == false) {
+            this.hasPlayed = true;
+            this.sound.play();
+            this.strokeWeight=15;
+        }
+    }
+
+    show() {
         push();
-        strokeWeight(3);
+
+        // stroke weight does the pulse
+        strokeWeight(this.strokeWeight);
+        if(this.strokeWeight>3) { this.strokeWeight--; }
         stroke(255);
-        fill(200);
+        if (this.isActive) {
+            fill("yellow");
+        } else {
+            fill(200);
+        }
         rectMode(CENTER);
         rect(this.x, this.y, 50, 50);
         pop();
